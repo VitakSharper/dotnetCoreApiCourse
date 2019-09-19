a41f66500daf56fdd87891cdf790447b45a84aeb
VitakSharper <v.banard@gmail.com>
2019-09-19 12:16:01 +0200

    First commit

---
+++ BookApiApp/models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookApiApp.models
+{
+    public class Category
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50, ErrorMessage = "The category name cannot be more than 50 characters")]
+        public string Name { get; set; }
+        public virtual ICollection<BookCategory> BookCategories { get; set; }
+
+    }
+}
---
+++ BookApiApp/models/Country.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookApiApp.models
+{
+    public class Country
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50, ErrorMessage = "The country name cannot be more than 50 characters")]
+        public string Name { get; set; }
+        public virtual ICollection<Author> Authors { get; set; }
+    }
+}
